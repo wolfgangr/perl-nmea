93f77178b9d95209fe9ef8f94061bead1d2e1a8a
Wolfgang Rosner <wrosner@tirnet.de>
2013-02-28 23:17:03 +0100

    build aggregation frame, count lines

--- prepare2holorev.pl
+++ prepare2holorev.pl
@@ -6,7 +6,7 @@
 
 $inbase = $ARGV[0] or die ("usage: $0 inputpath/base");
 
-$infilefilter = $inbase  . "_???.data";
+$infilefilter = $inbase  . "_[0-9][0-9][0-9].data";
 @infiles = `ls -1 $infilefilter` or die ("no input file match");
 
 
@@ -21,23 +21,64 @@ while (-e $outfile) {
 open (OUTFILE , ">".$outfile) || die ("cannot open outfile $outfile");
 
 my $lines_tot = 0;
+my $lines_tot_out= 0;
+
 foreach $infile (@infiles) {
 	chomp $infile;
 	print "$infile ...  ";
 
 	open (INFILE, $infile) || die ("cannot read from file $infile");
 	my $lines_in = 0;
+	my $lines_skipped =0;
+	my $lines_agg = 0; 
+	my $lines_out = 0;
+
+	my $ele_prev =  undef;
+	my$azi_prev = undef;
+
 	while (<INFILE>) {
 		# print $_;
 		$lines_in ++;	
-		$lines_tot ++;
+		$lines_tot_in ++;
 		@inlist = split ; #  ($_);
 		# print (join ("|", @inlist ), "\n");
 		unless ($#inlist == 5) { die ("format of $infile does not match"); }
 		# die ("cutting edge");
 		my ($t, $ele_int, $azi_int, $snr, $ele_frac, $azi_frac) = @inlist;
+		
+		# skip if no valid snr, elev. or azim. value
+		unless ($snr > 1) { $lines_skipped ++ ; next; }
+		unless ($ele_int >= 1) { $lines_skipped ++ ; next; }
+		unless ($azi_int >= 1) { $lines_skipped ++ ; next; }
+
+		# initialize if necessary
+		unless (defined ($ele_prev) and defined ($azi_prev) ) {
+			$ele_prev = $ele_int;
+			$azi_prev = $azi_int;
+			%snr_list = {};
+		}
+
+		if ($ele_prev == $ele_int and $azi_prev == $azi_int) {
+			# continue collecting data
+			$lines_agg ++;
+
+		} else {
+			$lines_out ++;
+			$lines_tot_out++;
+
+			# force init sequence as in start
+			$ele_prev =  undef ; # $ele_int;
+			$azi_prev = undef ; # $azi_int;		
+		}
+
+
+
 	}
 	# die ("cutting edge");
-	printf ("%d lines, %d total \n", $lines_in, $lines_tot);
+	printf ("\n\t%d lines in, %d skipped, %d aggregated, %d out, total %d in, %d out \n", 
+		$lines_in, $lines_skipped, $lines_agg, $lines_out, 
+		$lines_tot_in, $lines_tot_out);
   		
 }
+
+die ("======= THE END ===========\n");
