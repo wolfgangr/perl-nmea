5fb4cbe562b22977351e4fa2de9e6659da945af0
Wolfgang Rosner <wrosner@tirnet.de>
2013-03-02 23:23:23 +0100

    building in main loop

--- holo_rev.pl
+++ holo_rev.pl
@@ -3,8 +3,10 @@
 # calculation of reverse holgoram using aggregated skyplot data as input
 #
 
-use Math::Complex;		# hope this does'nt hurt performacne .... 
+use Math::Complex; #':pi' ;	# hope this does'nt hurt performacne .... 
 use Data::Dumper;
+
+# use constant PI    => 4 * atan2(1, 1); # pi is defined in Math::Complex
 
 @x_range = (-10..10);		# +x -> North
 @y_range = (-10..10);		# +y -> East
@@ -23,6 +25,7 @@ $dB1 = 30;			# dB value which is mapped to 1
 $scale = 0.001;		# rescaler to avoid numerical overflow etc
 
 # ========== END OF CONFIG =======================
+
 
 
 $infile = $ARGV[0] or die ("usage: $0 input file");
@@ -68,7 +71,7 @@ foreach (@x_range) {
 
 open (INFILE, $infile) || die ("cannot read from file $infile");
 	while (<INFILE>) {
-		# print $_;
+		print $_;
 		# eg 34.255548 100.524229 20 2
 		# elevation, azimuth, snr, repeats
 		@inlist = split ; #  ($_);
@@ -76,11 +79,57 @@ open (INFILE, $infile) || die ("cannot read from file $infile");
 		unless ($#inlist == 3) { die ("format of $infile does not match"); }
 		# die ("cutting edge");
 		my ($ele, $azi, $snr, $repeats) = @inlist;
-		die ("#============~~~~~~~~~~~~~~~~---------- <- cutting edge\n");
+		my $amplitd = $scale * $repeats * (10 ** (($snr - $dB1) / 10) ) ;
 
+		my $ele_rad = $ele * pi / 180;
+		my $azi_rad = $azi * pi / 180;
+
+		# wave number counts between adjacent voxels in each direction
+		my $x_wcnt = ($x_step / $lambda) * cos($ele_rad) * cos($azi_rad);
+		my $y_wcnt = ($y_step / $lambda) * cos($ele_rad) * sin($azi_rad);
+		my $z_wcnt = ($z_step / $lambda) * sin($ele_rad) ;
+
+		printf( "%s %s %s %s %s %s\n", $amplitd, $ele_rad,  $azi_rad ,
+			$x_wcnt, $y_wcnt, $z_wcnt ) ;
+
+		# the complex wave step factors of size 1;
+		my $x_wstp = cplxe(1, pi * 2 * $x_wcnt);
+		my $y_wstp = cplxe(1, pi * 2 * $y_wcnt);
+		my $z_wstp = cplxe(1, pi * 2 * $z_wcnt);
+
+		# printf ("x: %s | y: %s | z: %s \n", $x_wstp,  $y_wstp, $z_wstp) ;
+		# printf Dumper([$x_wstp,  $y_wstp, $z_wstp]) ;
+
+		# enforce cartesification, should be tested for performance effect...
+		$x_wstp->display_format('cartesian');
+		$y_wstp->display_format('cartesian');
+		$z_wstp->display_format('cartesian');
+
+		# printf ("x: %s | y: %s | z: %s \n", $x_wstp,  $y_wstp, $z_wstp) ;
+		# printf Dumper([$x_wstp,  $y_wstp, $z_wstp]) ;
+
+
+		foreach my $ix (0..$#x_range) {
+			my $x = $x_range [$ix] ;
+
+			foreach my $iy (0..$#y_range) {
+				my $y = $y_range [$iy] ;
+
+				foreach my $iz (0..$#z_range) {
+					my $z = $z_range [$iz] ;
+					printf ("x: %s -> %s, y: %s -> %s z: %s -> %s\n",
+						$ix, $x, $iy, $y, $iz, $z);
+
+		# die ("#============~~~~~~~~~~~~~~~~---------- <- cutting edge\n");
+
+				} 
+			} 
+		}
+		die ("#============~~~~~~~~~~~~~~~~---------- <- cutting edge I\n");
 	}
 close INFILE;
 
+die ("#============~~~~~~~~~~~~~~~~---------- <- cutting edge II\n");
 
 # =========================================
 
