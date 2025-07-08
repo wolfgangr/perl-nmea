f61301dd1137b430c207b942f8e2b1544a3e2474
wrosner <wrosner@kellerkind.rosner.lokal>
2025-07-08 13:28:00 +0200

    parse lat and lon fields

--- get_center.pl
+++ get_center.pl
@@ -15,14 +15,26 @@ use Data::Dumper ;
 # read from file in @ARGV
 while (<>) {
    chomp ; chop ;
-   if( /^\$G([NPLBAQ])(GGA),(.*)(\*..)$/  ) {
+   if( /^\$G([NPLBAQ])(GGA),(.*)(\*..)$/  ) { # process only GGA lines
       my @fields = split (',' , $3);
       print "\n";
       print scalar @fields ;
+
+      # parse time like hhmmss.ssi 194832.000, millisecs may vary
       my($hh, $mm, $ss) = (  $fields[0] =~ /^(\d{2})(\d{2})(\d{2}\.?\d{,4})$/ );
-      printf("time string: %s -> hr: %d, min: %d, sec: %s", $fields[0], $hh, $mm, $ss);
+      printf("time string: %s -> hr: %d, min: %d, sec: %s | ", $fields[0], $hh, $mm, $ss);
+
+      # parse lat / lon ddmm.mmmmmmmm
+      my($lat_deg, $lat_min) = ( $fields[1] =~ /^(\d{2,})(\d{2}\.\d+)$/ );
+      printf("lat: %s -> deg:%d, min: %s | ", $fields[1], $lat_deg, $lat_min );
+      # TBD: process South and West - no test data
+      my($lon_deg, $lon_min) = ( $fields[3] =~ /^(\d{2,})(\d{2}\.\d+)$/ );
+      printf("lon: %s -> deg:%d, min: %s | ", $fields[1], $lon_deg, $lon_min );
+
+
+
       print "\n";
-   } else {
+   } else { # other line than $G*GGA
 	print '.';
    }
 	
