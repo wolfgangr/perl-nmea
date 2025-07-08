90e9460f95d24b84c982b6fb43be4d56fbfca6c4
wrosner <wrosner@kellerkind.rosner.lokal>
2025-07-08 15:10:37 +0200

    collect min / max

--- get_center.pl
+++ get_center.pl
@@ -10,7 +10,9 @@
 use warnings;
 use strict;
 
-use Data::Dumper ;
+# use Data::Dumper ;
+use List::Util qw( min max );
+
 # ====================
 # config
 
@@ -37,18 +39,25 @@ my $lon_0 = 12.25;
 my $lat_cnt  = 0;
 my $lat_sum  = 0;
 my $lat_2sum = 0;
+my $lat_min =  9999;
+my $lat_max = -9999;
 
 my $lon_cnt  = 0;
 my $lon_sum  = 0;
 my $lon_2sum = 0;
+my $lon_min =  9999;
+my $lon_max = -9999;
+
 
 my $alt_cnt  = 0;
 my $alt_sum  = 0;
 my $alt_2sum = 0;
+my $alt_min =  99999;
+my $alt_max = -99999;
 
 my $skip_cnt = 0; # lines not matching G*GGA
 my $noq_cnt  = 0; # lines matching G*GGA but not quality pattern
-my$err_cnt  = 0; # lines matching G*GGA but not being parsable
+my $err_cnt  = 0; # lines matching G*GGA but not being parsable
 
 while (<>) {
    chomp ; chop ;
@@ -62,11 +71,11 @@ while (<>) {
       printf("time string: %s -> hr: %d, min: %d, sec: %s | ", $fields[0], $hh, $mm, $ss);
 
       # parse lat / lon ddmm.mmmmmmmm
-      my($lat_deg, $lat_min) = ( $fields[1] =~ /^(\d{2,})(\d{2}\.\d+)$/ );
-      printf("lat: %s -> deg:%d, min: %s | ", $fields[1], $lat_deg, $lat_min );
+      my($lat_deg, $lat_dmin) = ( $fields[1] =~ /^(\d{2,})(\d{2}\.\d+)$/ );
+      printf("lat: %s -> deg:%d, min: %s | ", $fields[1], $lat_deg, $lat_dmin );
       # TBD: process South and West - no test data
-      my($lon_deg, $lon_min) = ( $fields[3] =~ /^(\d{2,})(\d{2}\.\d+)$/ );
-      printf("lon: %s -> deg:%d, min: %s | ", $fields[1], $lon_deg, $lon_min );
+      my($lon_deg, $lon_dmin) = ( $fields[3] =~ /^(\d{2,})(\d{2}\.\d+)$/ );
+      printf("lon: %s -> deg:%d, min: %s | ", $fields[1], $lon_deg, $lon_dmin );
 
       my $qual = $fields[5];
       unless (defined $qselect{$qual}) {
@@ -81,8 +90,8 @@ while (<>) {
       # printf("q: %1s sv: %d hdop: %s alt: %s age: %s | ", 
       #       $qual,  $sats, $hdop,   $alt , $age);
             
-      my $lat = $lat_deg + $lat_min/60;
-      my $lon = $lon_deg + $lon_min/60;
+      my $lat = $lat_deg + $lat_dmin/60;
+      my $lon = $lon_deg + $lon_dmin/60;
 
       unless (defined $lat && defined $lon && defined $alt) {
          $err_cnt++;
@@ -93,14 +102,20 @@ while (<>) {
       $lat_cnt++;
       $lat_sum  += $lat;
       $lat_2sum += $lat * $lat;
+      $lat_min = min ($lat_min, $lat);
+      $lat_max = max ($lat_max, $lat);
 
       $lon_cnt++;
       $lon_sum  += $lon;
       $lon_2sum += $lon * $lon;
+      $lon_min = min ($lon_min, $lon);
+      $lon_max = max ($lon_max, $lon);
 
       $alt_cnt++;
       $alt_sum  += $alt;
       $alt_2sum += $alt * $alt;
+      $alt_min = min ($alt_min, $alt);
+      $alt_max = max ($alt_max, $alt);
 
       print "\n";
    } else { # other line than $G*GGA
@@ -114,5 +129,8 @@ while (<>) {
 
 print "\n";
 printf ("lat - cnt: %d  - sum: %f - sum of squares: %e \n", $lat_cnt, $lat_sum, $lat_2sum); 
-printf ("lon - cnt: %d  - sum: %f - sum of squares: %e \n", $lon_cnt, $lon_sum, $lon_2sum);
+printf ("\tmin: %.10f - max: %.10f \n", $lat_min, $lat_max);
+printf ("lon - cnt: %d  - sum: %f -isum of squares: %e \n", $lon_cnt, $lon_sum, $lon_2sum);
+printf ("\tmin: %.10f - max: %.10f \n", $lon_min, $lon_max);
 printf ("alt - cnt: %d  - sum: %f - sum of squares: %e \n", $alt_cnt, $alt_sum, $alt_2sum);
+printf ("\tmin: %.4f - max: %.4f \n", $alt_min, $alt_max);
